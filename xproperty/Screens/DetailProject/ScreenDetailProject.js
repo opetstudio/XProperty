d9a10645e2c8154228e95b4b12e8d93276a39e2b
Nofrets Poai <opetstudio@gmail.com>
2019-11-20 10:24:30 +0700

    done config google map

--- xproperty/Screens/DetailProject/ScreenDetailProject.js
+++ xproperty/Screens/DetailProject/ScreenDetailProject.js
@@ -18,10 +18,12 @@ import {
 } from 'native-base'
 // import { Grid, Row, Col } from 'react-native-easy-grid'
 import { StatusBar, Image, View } from 'react-native'
+import MapView from 'react-native-maps'
 import { Images } from '../../Themes'
 import Carousel from '../../Components/Carousel'
 
 const cardImage = Images.cardImage2
+const logo = Images.logo
 class ScreenDetailProject extends Component {
   render () {
     return (
@@ -42,6 +44,42 @@ class ScreenDetailProject extends Component {
           <View style={{ height: 300 }}>
             <Carousel />
           </View>
+          <Text style={{ alignSelf: 'center', margin: 10 }}>Project SatuDua Tiga Empatlima enamtujuh</Text>
+          <MapView
+            style={{ height: 300, width: '100%' }}
+            region={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.015,
+              longitudeDelta: 0.0121
+            }}
+          />
+          <Card>
+            <CardItem bordered>
+              <Left>
+                <Thumbnail source={logo} />
+                <Body>
+                  <Text>Project SatuDua Tiga Empatlima enamtujuh</Text>
+                  <Text note>Jl. Benyamin Sueb No. 21, Kemayoran, Jakarta Utara</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem bordered>
+              <Body>
+                <Text style={{ alignSelf: 'center', marginBottom: 5 }}>Description</Text>
+                <Text note>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+              </Body>
+            </CardItem>
+            <CardItem style={{ paddingVertical: 0 }}>
+              <Left>
+                <Button transparent>
+                  <Icon name='logo-github' />
+                  <Text>4,923 stars</Text>
+                </Button>
+              </Left>
+            </CardItem>
+          </Card>
+
         </Content>
       </Container>
     )
